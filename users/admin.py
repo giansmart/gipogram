9283b0ffab8f7954d08400124cb698fa2cfcbbce
giansmart <giancarlo.poemape@hotmail.com>
2021-06-27 23:08:15 -0500

    adding module inline

--- users/admin.py
+++ users/admin.py
@@ -2,9 +2,11 @@
 
 #Django
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 #Models
 from users.models import Profile
+from django.contrib.auth.admin import User
 
 
 # Register your models here.
@@ -18,3 +20,27 @@ class ProfileAdmin(admin.ModelAdmin):
     list_editable = ('phone_number','website','picture')
     search_fields = ('user__email','user__first_name','user__last_name','phone_number')
     list_filter = ('created','modified','user__is_active','user__is_staff')
+
+    fieldsets = (
+        ('Profile', {'fields': (('user','picture'),),}),
+        ('Extra Info', {'fields': (('website','phone_number'),('biography'))}),
+        ('Metadata',{'fields': (('created'),('modified'))}),
+    )
+
+    readonly_fields = ('created','modified','user')
+
+class ProfileInLine(admin.StackedInline):
+    """Profile inline admin for users"""
+
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    """Adds profile admin to base user admin"""
+    inlines = (ProfileInLine,)
+    list_display = ('username','email','first_name','last_name','is_active','is_staff')
+
+admin.site.unregister(User)
+admin.site.register(User,UserAdmin)
